6d33491cf2fadda1eda240ebc9c8f14f8b099c5b
Buka76 <evgeniakurteeva6984@gmail.com>
2023-05-15 22:32:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,98 @@
+﻿// ConsoleApplication10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+class TBook {
+public:
+	string Name;
+	string Author;
+	int Count;
+};
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	const int s = 3;
+	TBook books[s];
+	ifstream fin;
+	fin.open("book.txt");
+	if (fin.is_open()) {
+		for (int i = 0; i < s; i++) {
+			string temp;
+			getline(fin, books[i].Name);
+			getline(fin, books[i].Author);
+			getline(fin, temp);
+			books[i].Count = stoi(temp);
+		}
+		fin.close();
+	}
+	else return 1;
+
+	string name;
+	cout << "Введите название книги: ";
+	getline(cin, name);
+	bool Thereis = false;
+	for (int i = 0; i < s; i++) {
+		if (books[i].Name == name) {
+			cout << "Name: " << books[i].Name << endl;
+			cout << "Author: " << books[i].Author << endl;
+			cout << "Count: " << books[i].Count << endl;
+			Thereis = true;
+			break;
+		}
+	}
+	if (!Thereis) {
+		cout << "Извините,такой книги нет!" << endl;
+	}
+
+	string author;
+	cout << "Введите имя автора: ";
+	cin >> author;
+	Thereis = false;
+	for (int i = 0; i < s; i++) {
+		if (books[i].Author == author) {
+			cout << "Name: " << books[i].Name << endl;
+			cout << "Author: " << books[i].Author << endl;
+			cout << "Count: " << books[i].Count << endl;
+			Thereis = true;
+			break;
+		}
+	}
+	if (!Thereis) {
+		cout << "Извините,такого автора нет!" << endl;
+	}
+
+	int count;
+	cout << "Введите количество книг: ";
+	cin >> count;
+	Thereis = false;
+	for (int i = 0; i < s; i++) {
+		if (books[i].Count == count) {
+			cout << "Name: " << books[i].Name << endl;
+			cout << "Author: " << books[i].Author << endl;
+			cout << "Count: " << books[i].Count << endl;
+			Thereis = true;
+			break;
+		}
+	}
+	if (!Thereis) {
+		cout << "Извините,такого количества книг нет!" << endl;
+	}
+
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
